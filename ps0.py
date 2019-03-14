6d709c50ccf238953e613fd1d1402debf0bf609d
Ferhat94 <45284847+Ferhat94@users.noreply.github.com>
2019-03-14 13:57:40 +0300

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 12:45:27 2019
+
+@author: FD
+"""
+import math
+x = int(input("Enter the number:"))
+y = int(input("Enter the number:"))
+print("x**y" , x**y)
+print("log(x)=" , math.log(x,2))
